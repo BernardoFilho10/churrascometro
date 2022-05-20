a581d28fc0ad4d2d8335e7df8101ede77ef1a0f9
BernardoFilho10 <bernardodev2022@gmail.com>
2022-05-19 21:24:10 -0300

    arrumando uns detalhes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,43 +2,36 @@
 // Cerveja - 1200 ml por pessoa + 6 horas - 200ml
 // Refrigerante-agua - 1000ml por pessoa + 6 horas 1500ml
 // crianças valem por 0,5
+
 const inputAdultos = document.getElementById("adultos");
 const inputCriancas = document.getElementById("criancas");
 const inputDuracao = document.getElementById("duracao");
 const resultado = document.getElementById("resultado");
 
 function calcular(){
-    
-
     const adultos = inputAdultos.value;
     const criancas = inputCriancas.value;
     const duracao = inputDuracao.value;
-
-
+    
     const quantidadeTotalCarne = carnePorPessoa(duracao) * adultos + (carnePorPessoa(duracao) / 2 * criancas); 
     const quantidadeTotalCerveja = cervejaPorPessoa(duracao) * adultos;
     const quantidadeTotalBebidas = bebidasPorPessoa(duracao) * adultos + (bebidasPorPessoa(duracao) / 2 * criancas);
 
     resultado.innerHTML = `<h2 class="result-info">Você vai precisar de:</h2>`
-    resultado.innerHTML +=  ` <div class="result-block">
-    <img src="./src/imagens/32380cutofmeat_98860.ico"/>
-    <p>${quantidadeTotalCarne / 1000}kg de Carne</p>
+    resultado.innerHTML +=  ` <div class="result-icon">
+    <img src="./src/imagens/carne.ico"/>
+    <p>${quantidadeTotalCarne / 1000} kg de Carne</p>
     </div> `;
       
-
-
-
-    resultado.innerHTML += ` <div class="result-block">
-    <img src="./src/imagens/2340291-beverage-drink-glass_85535.ico"/>
+    resultado.innerHTML += ` <div class="result-icon">
+    <img src="./src/imagens/cerveja.ico"/>
     <p>${Math.ceil(quantidadeTotalCerveja / 355)} latas de 
     Cerveja</p>
     </div> `;
 
-
-
-    resultado.innerHTML += ` <div class="result-block">
-    <img src="./src/imagens/sodacokebottleicon_97040.ico"/>
-    <p>${Math.ceil(quantidadeTotalBebidas / 2000)}Pet's de 2l de Bebidas</p>
+    resultado.innerHTML += ` <div class="result-icon">
+    <img src="./src/imagens/soda_drink.ico"/>
+    <p>${Math.ceil(quantidadeTotalBebidas / 2000)} Pet's de 2l de Bebidas</p>
     </div> `;
 }
 
